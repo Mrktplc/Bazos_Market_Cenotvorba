77973fd5baee32798c9821eca51cba90400571e3
Mrktplc <46596110+Mrktplc@users.noreply.github.com>
2019-02-21 16:35:38 +0100

    To do: Nulova cena,lock, verzia, opravit zapis, vyhladavanie

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
-  StdCtrls;
+  StdCtrls, ExtCtrls;
 
 type
   tovar=record
@@ -18,10 +18,21 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Ponuka: TStringGrid;
+    Edit1: TEdit;
+    Memo2: TMemo;
+    Ulozit: TButton;
+    Image1: TImage;
+    Memo1: TMemo;
+    Cena: TStringGrid;
     procedure FormCreate(Sender: TObject);
+    procedure CenaClick(Sender: TObject);
     procedure Reload;
+    procedure UlozitClick(Sender: TObject);
     procedure Zapis;
+    function privelkaNC(iTovaru, novaNC:integer):boolean;
+    function nizkaPC(iTovaru, novaPC:integer):boolean;
+    procedure zmenaNC;
+    procedure zmenaPC;
   private
     { private declarations }
   public
@@ -44,13 +55,14 @@ procedure TForm1.FormCreate(Sender: TObject);
 var i, pom:integer;
   pom_s:string;
 begin
-//image1.picture.LoadFromFile('logo.bmp');
-  AssignFile(subor,'tovar3.txt');
+image1.picture.LoadFromFile('logo.png');
+  AssignFile(subor,'tovar4.txt');
   Reset(subor);
   Readln(subor,pom_s);
  //////////////////////
  //Nacitanie
  pocet_riad:=strtoint(pom_s);
+ Memo1.Append(inttostr(pocet_riad));
    For i:=1 to pocet_riad do
        begin
          readln(subor,pom_s);
@@ -75,27 +87,56 @@ begin
  //Vsuva do tabulky
  Reload;
 end;
+
+procedure TForm1.CenaClick(Sender: TObject);
+var  S1,S2:integer;
+  P1,P2:string;
+begin
+ P1:=inttostr(Cena.Col);    //zisti mi poziciu vertik
+ P2:=inttostr(Cena.Row);    //zisti mi poziciu horizontalne
+ S1:=strtoint(P1);
+ Memo1.Append(P1+'___'+P2);                            //pouzit If,
+
+ IF 3 = S1 then
+    begin
+     ZmenaNC;
+     exit;
+    end;
+ IF 4 = S1 then
+    Begin
+     ZmenaPC;
+     exit;
+    end;
+end;
+
 procedure TForm1.Reload;
 var i,j:integer;
   c1,c2:real;
 begin
-     For i:=1 to pocet_riad do   //zapis do riadkov
-     begin
-         //tahanie z pola podla postupu
-         c1:=(tovary[i].ncena) / 100;
-         c2:=(tovary[i].pcena) / 100;
-         Ponuka.Cells[0,i]:= IntToStr(i);
-         Ponuka.Cells[1,i]:=IntToStr(tovary[i].kod);
-         Ponuka.Cells[2,i]:=tovary[i].nazov;
-         Ponuka.Cells[3,i]:=FormatFloat('0.## €',(c1));
-         Ponuka.Cells[4,i]:=FormatFloat('0.## €',(c2));
-     end;
+  For i:=1 to pocet_riad do
+      begin
+        c1:=(tovary[i].ncena) /100;
+        c2:=(tovary[i].pcena) /100;
+        Cena.Cells[0,i]:=Inttostr(i);
+        Cena.Cells[1,i]:=inttostr(tovary[i].kod);
+        Cena.Cells[2,i]:=tovary[i].nazov;
+        Cena.Cells[3,i]:=FormatFloat('0.## €',(c1));
+        Cena.Cells[4,i]:=FormatFloat('0.## €',(c2));
+        Memo2.Append(floattostr((c1)));
+      end;
+
 end;
+
+procedure TForm1.UlozitClick(Sender: TObject);
+begin
+  Zapis;
+end;
+
 procedure TForm1.Zapis;
 var i:integer;
     t1,t2,t3,t4:string;
 begin
- AssignFile(subor,'cennik.txt');
+ AssignFile(subor,'cennik2.txt');
  Rewrite(subor);
  Writeln(subor,pocet_riad);
  For i:=1 to pocet_riad do
@@ -104,10 +145,109 @@ begin
          t2:=tovary[i].nazov;
          t3:=floattostr(tovary[i].ncena);
          t4:=FloatToStr(tovary[i].pcena);
-         writeln(subor, t1+';'+t2+';'+t3+';'+t4);
+         writeln(subor, t1+';'+t3+';'+t4);
      end;
 
 end;
-
+function TForm1.privelkaNC(iTovaru, novaNC:integer):boolean;
+begin
+ IF novaNC > tovary[iTovaru].pcena
+    then begin
+      Showmessage('Zadaj nizsiu cenu');
+      exit(true);
+      end else begin
+          exit(false);
+    end;
+end;
+function TForm1.nizkaPC(iTovaru, novaPC:integer):boolean;
+begin
+ IF novaPC > tovary[iTovaru].ncena
+    then begin
+      Showmessage('Zadaj nizsiu cenu');
+      exit(true);
+      end else begin
+          exit(false);
+    end;
+end;
+procedure TForm1.zmenaNC;
+var
+   QueryResult,success:Boolean;
+  UserString,P2:string;
+  number,i:integer;
+  jePrivelkaNC:boolean;
+begin
+ jePrivelkaNC:=false;
+///////////////////////////////
+ P2:=inttostr(Cena.Row);   //do pozicie P1 som si nahral hor cisla,
+ i:=strtoint(P2);            //do i vkladam udaje z pozicky 1, neskor i pouzivam pri tovare aby som vedel kde som to zmenil
+//Memo1.Append(inttostr(i));
+///////////////////////////////
+  if InputQuery('Nova cena', 'Zadaj novu cenu', UserString) = True
+      then
+       begin
+         success:=TryStrtoInt(UserString, number);                                     //odobrit nech zostane predtym zadana cena
+         if not success then begin
+          ShowMessage('Zadaj cenu, nie abecedu!');
+          exit;
+         end;
+       end
+     else begin
+          exit;
+     end;
+/////////////////////////////////////
+jePrivelkaNC:=privelkaNC(i, number);
+if jePrivelkaNC then begin
+ exit;
+ end;
+//////////////////////////////////
+if number > 0
+    then
+     tovary[i].ncena:=number
+     else
+       ShowMessage('Zadaj vyssiu cenu');
+/////////////////////////////////
+ Reload;
+ Zapis;
+end;
+procedure TForm1.ZmenaPC;
+var
+   QueryResult,success:Boolean;
+  UserString,P2:string;
+  number,i:integer;
+  jeNizkaPC:boolean;
+begin
+ jeNizkaPC:=false;
+///////////////////////////////
+ P2:=inttostr(Cena.Row);   //do pozicie P1 som si nahral hor cisla,
+ i:=strtoint(P2);            //do i vkladam udaje z pozicky 1, neskor i pouzivam pri tovare aby som vedel kde som to zmenil
+//Memo1.Append(inttostr(i));
+///////////////////////////////
+  if InputQuery('Nova cena', 'Zadaj novu cenu', UserString) = True
+      then
+       begin
+         success:=TryStrtoInt(UserString, number);                                     //odobrit nech zostane predtym zadana cena
+         if not success then begin
+          ShowMessage('Zadaj cenu, nie abecedu!');
+          exit;
+         end;
+       end
+     else begin
+          exit;
+     end;
+/////////////////////////////////////
+jeNizkaPC:=nizkaPC(i, number);
+if jeNizkaPC then begin
+ exit;
+ end;
+//////////////////////////////////
+if number > 0
+    then
+     tovary[i].ncena:=number
+     else
+       ShowMessage('Zadaj vyssiu cenu');
+/////////////////////////////////
+ Reload;
+ Zapis;
+end;
 end.
 
